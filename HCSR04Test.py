eb3ef588f9332ad650631f19c9f173c120f76192
ashish <ashish.joshi@mindstix.com>
2018-06-29 21:18:55 +0530

    modified sampling rate

--- HCSR04Test.py
+++ HCSR04Test.py
@@ -15,7 +15,7 @@ GPIO.setwarnings(False)
 try:
     while True:
         print("Distance: %.2fcm" %(hcsr.distance(-0.5))) # Offset to -0.5 | Round to the second decimal
-        time.sleep(4)
+        time.sleep(10)
 
 except KeyboardInterrupt:
     GPIO.cleanup()
