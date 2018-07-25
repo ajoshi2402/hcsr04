35de4b1b57606567749f18bb3ad7221a4ccfbf02
ashish <ashish.joshi@mindstix.com>
2018-07-25 16:18:56 +0530

    added Thingspeak cloud

--- HCSR04Test.py
+++ HCSR04Test.py
@@ -6,16 +6,23 @@
 #
 # Use this script as an example.
 
+import sys
 import RPi.GPIO as GPIO
 import HCSR04Ultrasonic as hcsr
 import time
+from time import sleep
+import urllib2
 
 GPIO.setwarnings(False)
 
 try:
     while True:
         print("Distance: %.2fcm" %(hcsr.distance(-0.5))) # Offset to -0.5 | Round to the second decimal
-        time.sleep(10)
+        baseURL = 'http://api.thingspeak.com/update?api_key=NVBW0ZTQ2W3BNTK2&field1='
+	f = urllib2.urlopen(baseURL +str(a))
+	f.read()
+	f.close()
+	time.sleep(5)
 
 except KeyboardInterrupt:
     GPIO.cleanup()
